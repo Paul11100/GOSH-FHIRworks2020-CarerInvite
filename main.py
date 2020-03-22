1b30d37f9f2950cfd2d6c930de9a9c3ea76dd835
Paul Lin <paullam11100@hotmail.com>
2020-03-22 15:50:28 +0000

    create format suitable for json output + requests

--- main.py
+++ main.py
@@ -7,28 +7,34 @@ class Main:
         self.fhir = FHIR('https://localhost:5001/api/', verify_ssl=False)
         self.patients = self.fhir.get_all_patients()
 
-    def print_patients_in_range(self, num_patients, lower, upper, sender_name, sender_ref, app_url):
+    def print_patients_in_range(self, limit, lower, upper, sender_name, sender_ref, app_url):
         i = 0
+        patients_list = []
         for patient in self.patients:
-            if not i < num_patients:
-                return
+            dict = {}
+            if not i < limit:
+                # print(patients_list)
+                return patients_list
             id = patient.uuid
             name = patient.full_name()
             age = patient.age()
             if age >= lower and age < upper:
                 telecoms = patient.telecoms
                 phone = telecoms[len(telecoms) - 1].number
-                print(phone)
+                # print(phone)
                 msg = "Hi " + name + ",\n" + "Are you a carer? If so, we've built an app just for you! You can download our app for carers here: " + app_url + "\nUse this reference code to set up your app: " + sender_ref + "\nTake Care,\n" + sender_name
-                print(msg)
-                print(id)
-                print(name)
-                print(age)
-                print()
+                # print(msg)
+                # print(id)
+                # print(name)
+                # print(age)
+                # print()
+                dict["phone"] = phone
+                dict["msg"] = msg
+                patients_list.append(dict)
                 i += 1
 
 main = Main()
-main.print_patients_in_range(3, 55, 65, "Paul", "UCL-FHIR-Hack", "appdownload.com")
+list = main.print_patients_in_range(3, 55, 65, "Paul", "UCL-FHIR-Hack", "appdownload.com")
 
 # patient = fhir.get_patient('8f789d0b-3145-4cf2-8504-13159edaa747')
 # name = patient.full_name()
--- server.py
+++ server.py
@@ -1,12 +1,17 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
+from main import Main
 
 app = Flask(__name__)
 api = Api(app)
 
 class Patients(Resource):
     def get(self):
-        result = {"hello" : "world", "bye": "bruh"}
+        main = Main()
+        limit = int(request.args.get("limit"))
+        print(limit)
+        result = main.print_patients_in_range(limit, 55, 65, "Paul", "UCL-FHIR-Hack", "appdownload.com")
+        # result = {"hello" : "world", "bye": "bruh"}
         return jsonify(result)
 
 api.add_resource(Patients, '/patients') #Route 1
