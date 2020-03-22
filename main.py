b737db8514f711d7f1df67f4f163888d5803f589
Paul Lin <paullam11100@hotmail.com>
2020-03-22 14:31:22 +0000

    change back main.py after Key_error is fixed

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fhir_parser import FHIR
 
-fhir = FHIR('https://localhost:5001/api/', verify_ssl=False)
+# fhir = FHIR('https://localhost:5001/api/', verify_ssl=False)
 
 class Main:
     def __init__(self):
         self.fhir = FHIR('https://localhost:5001/api/', verify_ssl=False)
-        # self.patients = self.fhir.get_all_patients()
+        self.patients = self.fhir.get_all_patients()
 
     def print_patients_in_range(self, lower, upper):
         for patient in self.patients:
@@ -22,7 +22,7 @@ class Main:
 main = Main()
 # main.print_patients_in_range(55, 65)
 
-patient = fhir.get_patient('8f789d0b-3145-4cf2-8504-13159edaa747')
-name = patient.full_name()
-print(patient)
-list = fhir.get_all_patients()
+# patient = fhir.get_patient('8f789d0b-3145-4cf2-8504-13159edaa747')
+# name = patient.full_name()
+# print(patient)
+# list = fhir.get_all_patients()
